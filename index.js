59166175923340359b121a7dc2ba3c92fee37945
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-08-20 15:45:59 +0300

    Modified gulp to bundle all files to bundle.js. Inlcuded that file in index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-import { App } from "./dist/App";
+import { App } from "./src/App";
+//const App = require("./dist/App");
 
 ReactDOM.render(<App />, document.getElementById('app'));
