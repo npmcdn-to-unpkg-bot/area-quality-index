2658bc67aebb57522205e5daeac9e2556c418617
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-08-20 17:36:04 +0300

    Added method that interacts with the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Map} from "./DisplayLocation";
+import $ from 'jquery';
 
 export class App extends React.Component {
   constructor(props) {
     super(props);
-    //this.updateSelectedAreaInfo = this.updateSelectedAreaInfo.bind(this);
+    this.updateSelectedAreaInfo = this.updateSelectedAreaInfo.bind(this);
     this.state = {
       areaData: {}
     }
-    //updateSelectedAreaInfo("20540");
+    this.updateSelectedAreaInfo("20540");
   }
   updateSelectedAreaInfo(location) {
     $.get("/api/area/" +location)
     .done((data) => {
-      setState({areaData: data});
+      this.setState({areaData: JSON.stringify(data)});
     });
   }
   render() {
     return (
       <div className="content">
-        <p>This one is alive! So hello there world!!!FobarQuuz dajshfj ksahjk nanan</p>
+        <p>This one is alive! So hello there world!!!FobarQuuz dajshfj ksahjk nanan 11</p>
+        {JSON.stringify(this.state.areaData)}
+        {}
       </div>
     )
   }
