d8bd071e32e24d9d5a947be9bf31fc0e6ab1d268
Einari Kurvinen <einari.kurvinen@gmail.com>
2016-08-20 15:32:50 +0300

    changed koa to express

--- server.js
+++ server.js
@@ -1,19 +1,74 @@
 'use strict';
 
-let koa     = require('koa'),
-    serve   = require('koa-static'),
-    router  = require('koa-router')();
+let express    = require('express');        
+let app        = express();                 
+let bodyParser = require('body-parser');; 
 
-router.get('/api/area/:code',
-    function *(next) {
-        console.log(this.params);
-        this.body = 'hello world';
-    }
-);
+let request		= require('request'); 
 
-let app = koa();
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(router.routes());
-app.use(serve(__dirname + '/public'));
-app.listen(3000);
-console.log('server running on port 3000');
+var port = process.env.PORT || 3000;        // set our port
+
+var router = express.Router();   
+
+router.get('/api/area/:code', function(req, res) {
+	console.log(req.params.code);
+	let zipcode = req.params.code;
+		// Basic data for the query
+	    let requestData = {
+				  "query": [
+				    {
+				      "code": "Postinumeroalue",
+				      "selection": {
+				        "filter": "item",
+				        "values": [
+				          zipcode
+				        ]
+				      }
+				    }
+				  ],
+				  "response": {
+				    "format": "json-stat"
+					}
+		}; 
+		request({
+	    	url: 'http://pxnet2.stat.fi/PXWeb/api/v1/fi/Postinumeroalueittainen_avoin_tieto/2016/paavo_9_koko_2016.px',
+	    	method: 'POST',
+	    	json: true,
+	    	body: requestData,
+
+	    }, (error, response, body) => {
+	        	if(error) {
+	        		console.log("no data, error", error);
+	        		res.json('no data', error);
+	        	}
+	        	res.json(response);
+	        }); 
+});
+
+router.get('/test', function(req, res) {
+	res.json({test: 'test'});
+});
+
+// router.get('/api/area/:code',
+//     function *(next) {
+//         console.log(this.params.code);
+//         var zipcode = this.params.code;
+//         // this.body = 'hello world';
+
+//         let data =  yield getData(zipcode);
+//         console.log("data should be here", data);
+//         this.body = "test: " + data; 
+//     }
+// );
+
+
+app.use('/', router);
+app.use(express.static(__dirname + '/'));
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
