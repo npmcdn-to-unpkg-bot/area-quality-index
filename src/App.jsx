dbc4f7e6870968a803c6cbf522ea8b90f52dbf05
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-08-20 12:40:08 +0300

    Some initial frontend stuff

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>This one is alive! So hello there world!</div>
+    )
+  }
+}
+
+export App;
