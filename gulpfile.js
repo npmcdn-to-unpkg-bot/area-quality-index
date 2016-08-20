3adee1ce7c7d7dcb47d733072474362c81c57ad0
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-08-20 13:53:18 +0300

    Added browserify to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const browserify = require('gulp-browserify');
 
 gulp.task('default', () => {
-    return gulp.src('src/*.jsx')
+    return gulp.src('index.js')
         .pipe(babel({
             presets: ['es2015', 'react']
         }))
+        .pipe(browserify())
         .pipe(gulp.dest('dist'));
 });
