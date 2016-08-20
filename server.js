be012d118be4a75e88fa55c6213591f98f02f306
mkaski <msjkas@utu.fi>
2016-08-20 12:36:08 +0300

    add server.js base

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let koa     = require('koa'),
+    serve   = require('koa-static'),
+    router  = require('koa-router')();
+
+router.get('/api/area/:code',
+    function *(next) {
+        console.log(this.params);
+        this.body = 'hello world';
+    }
+);
+
+let app = koa();
+
+app.use(router.routes());
+app.use(serve(__dirname + '/public'));
+app.listen(3000);
+console.log('server running on port 3000');
