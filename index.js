29f38a3de64a24faa48e669d1e5d8eaf5e7ab447
Simo-Pekka Koskinen <simo-pekka@vaadin.com>
2016-08-20 15:47:56 +0300

    Removed unnecesasry file

--- index.js
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-import { App } from "./src/App";
-//const App = require("./dist/App");
-
-ReactDOM.render(<App />, document.getElementById('app'));
